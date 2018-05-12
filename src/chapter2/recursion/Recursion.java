4b744af8331833881c353c8c6787849ea1b2e57a
ccrazy386 <ccrazy386@gmail.com>
2018-05-12 20:31:08 +0900

    2장 재귀와 백트래킹 팩토리얼 예제 코드

---
+++ src/chapter2/recursion/Recursion.java
@@ -0,0 +1,20 @@
+package chapter2.recursion;
+
+public class Recursion {
+    public static void main(String[] args) {
+        // Test
+        System.out.println(Fact(5));
+    }
+
+    // 양의 정수 팩토리얼 계산
+    private static int Fact (int n) {
+        if (n == 1) {
+            return 1;
+        } else if (n == 0) {
+            return 1;
+        } else  {
+            // 재귀 경우 : (n - 1) 팩토리얼에 n을 곱한다.
+            return Fact(n - 1) * n;
+        }
+    }
+}
