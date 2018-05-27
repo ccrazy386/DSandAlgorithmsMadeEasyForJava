3a058e76b5366552b35358746510eb99274859f9
ccrazy386 <ccrazy386@gmail.com>
2018-05-27 14:27:57 +0900

    5장 큐 > 연결 리스트 구현

---
+++ src/chapter5/queue/LLQueue.java
@@ -0,0 +1,54 @@
+package chapter5.queue;
+
+import chapter4.Stack.LLNode;
+
+public class LLQueue {
+
+    private LLNode frontNode;
+    private LLNode rearNode;
+
+    private LLQueue() {
+        this.frontNode = null;
+        this.rearNode = null;
+    }
+
+    public static LLQueue createQueue() {
+        return new LLQueue();
+    }
+
+    public boolean isEmpty() {
+        return (frontNode == null);
+    }
+
+    public void enQueue(Integer data) {
+        LLNode newNode = new LLNode(data);
+
+        if (rearNode != null) {
+            rearNode.setNext(newNode);
+        }
+
+        rearNode = newNode;
+
+        if (frontNode == null) {
+            frontNode = rearNode;
+        }
+    }
+
+    public Integer deQueue() {
+        Integer data = null;
+        if (isEmpty()) {
+            throw new IllegalStateException("Queue Empty");
+        } else {
+            data = frontNode.getData();
+            frontNode = frontNode.getNext();
+        }
+        return data;
+    }
+
+    public static void main(String[] args) {
+        LLQueue llQueue = createQueue();
+
+        llQueue.enQueue(10);
+        System.out.println(llQueue.isEmpty());
+    }
+}
