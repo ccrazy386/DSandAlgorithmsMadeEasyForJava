f5e22083a81881a9592efcec82979dd6e0e4339d
ccrazy386 <ccrazy386@gmail.com>
2018-06-17 16:12:15 +0900

    6장 트리 > 이진트리 > 전위 탐색(비재귀방식 책 자체코드에 오류)

--- src/chapter6/Trees/BinaryTreeNode.java
+++ src/chapter6/Trees/BinaryTreeNode.java
@@ -65,4 +65,25 @@ public class BinaryTreeNode {
         }
         return;
     }
+
+    public static void main(String[] args) {
+        BinaryTreeNode rootNode = new BinaryTreeNode(1);
+        BinaryTreeNode node2 = new BinaryTreeNode(2);
+        BinaryTreeNode node3 = new BinaryTreeNode(3);
+        BinaryTreeNode node4 = new BinaryTreeNode(4);
+        BinaryTreeNode node5 = new BinaryTreeNode(5);
+        BinaryTreeNode node6 = new BinaryTreeNode(6);
+        BinaryTreeNode node7 = new BinaryTreeNode(7);
+
+        rootNode.setLeft(node2);
+        rootNode.setRight(node3);
+
+        node2.setLeft(node4);
+        node2.setRight(node5);
+
+        node3.setLeft(node6);
+        node3.setRight(node7);
+
+        rootNode.PreOrder(rootNode);
+    }
 }
